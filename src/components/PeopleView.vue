910f73c0efad757367b01f2ff286cc3718ca216f
david <davidsanchezdiaz72@gmail.com>
2024-04-04 09:58:54 +0200

    Agregada lógica y visualización de la vista de personas

--- src/components/PeopleView.vue
+++ src/components/PeopleView.vue
@@ -1,9 +1,18 @@
 <script setup>
 
+    import { getGrandTotal, store } from "../store/store";
+
 </script>
 
 <template>
-
+    <div class="no-items" v-if="store.people.length === 0">No items</div>
+    <div class="people-view" v-if="store.people.length > 0">
+        <div class="header">
+            <div>
+                <div>{{ getGrandTotal() }}</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
