be31ca1fde2651b4ac325371cc99e6e4d5a5ec9c
david <davidsanchezdiaz72@gmail.com>
2024-04-04 10:00:35 +0200

    Añadir comentarios

--- src/components/PeopleView.vue
+++ src/components/PeopleView.vue
@@ -1,14 +1,18 @@
 <script setup>
-
+    // Importar la función getGrandTotal y el objeto store desde el archivo de la tienda
     import { getGrandTotal, store } from "../store/store";
-
 </script>
 
 <template>
+    <!-- Mostrar mensaje "No items" si no hay elementos en la lista de personas -->
     <div class="no-items" v-if="store.people.length === 0">No items</div>
+
+    <!-- Mostrar la vista de personas si hay elementos en la lista de personas -->
     <div class="people-view" v-if="store.people.length > 0">
+        <!-- Encabezado de la vista de personas -->
         <div class="header">
             <div>
+                <!-- Mostrar el resultado del total obtenido a través de la función getGrandTotal() -->
                 <div>{{ getGrandTotal() }}</div>
             </div>
         </div>
@@ -16,5 +20,5 @@
 </template>
 
 <style scoped>
-
+    /* Estilos específicos del componente */
 </style>
