61efaa4f525a89106af22bdf4d86179daafd3a73
david <davidsanchezdiaz72@gmail.com>
2024-04-05 10:11:48 +0200

    Crear componente Label para mostrar título y valor formateado como moneda

---
+++ src/components/Label.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { defineProps } from "vue";
+
+    const props = defineProps(['title', 'value']);
+
+</script>
+
+<template>
+        <div>
+            <span class="title">{{ props.title }}</span>
+            <span class="value">{{
+                new Intl.NumberFormat('en-Us',{
+                    style: 'currency',
+                    currency: 'USD'
+                }).format(props.value)
+            }}</span>
+        </div>
+</template>
+
+<style scoped>
+
+</style>
