c380bd33b6fe6a2b45fd82e16105424d94439892
david <davidsanchezdiaz72@gmail.com>
2024-04-05 10:16:28 +0200

    Añadir comentarios

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,22 +1,27 @@
 <script setup>
+    // Importar la función defineProps desde Vue para definir las propiedades del componente
     import { defineProps } from "vue";
 
+    // Definir las propiedades 'title' y 'value' del componente
     const props = defineProps(['title', 'value']);
-
 </script>
 
 <template>
-        <div>
-            <span class="title">{{ props.title }}</span>
-            <span class="value">{{
-                new Intl.NumberFormat('en-Us',{
-                    style: 'currency',
-                    currency: 'USD'
-                }).format(props.value)
-            }}</span>
-        </div>
+    <!-- Estructura del componente para mostrar un título y un valor formateado como moneda -->
+    <div>
+        <!-- Mostrar el título utilizando la propiedad 'title' definida en las props -->
+        <span class="title">{{ props.title }}</span>
+        <!-- Mostrar el valor formateado como moneda utilizando la propiedad 'value' definida en las props -->
+        <span class="value">{{
+            // Formatear el valor utilizando el objeto Intl.NumberFormat para mostrarlo como moneda en formato USD
+            new Intl.NumberFormat('en-Us',{
+                style: 'currency',
+                currency: 'USD'
+            }).format(props.value)
+        }}</span>
+    </div>
 </template>
 
 <style scoped>
-
+    /* Estilos específicos del componente */
 </style>
